8eb150b00adcde218de49421c8cb4e78608a5490
myw35586 <129239080+myw35586@users.noreply.github.com>
2026-08-24 10:56:56 +0800

    Create Visualization of medicine.py

---
+++ Training and Generation/Visualization of medicine.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import AllChem
+
+
+csv_path = "/home/myw/drugvae/3D_model/data/multi_point_results.csv"
+df = pd.read_csv(csv_path)
+
+
+top_mols_df = df.head(9)
+
+mols = []
+legends = []
+
+for index, row in top_mols_df.iterrows():
+    smi = row['Full_Molecule']
+    qed = row['QED']
+    mol = Chem.MolFromSmiles(smi)
+
+    AllChem.Compute2DCoords(mol)
+    mols.append(mol)
+    
+
+    legends.append(f"Rank {index+1}\nQED: {qed:.3f}")
+
+
+img = Draw.MolsToGridImage(
+    mols, 
+    molsPerRow=3, 
+    subImgSize=(300, 300), 
+    legends=legends,
+    returnPNG=False
+)
+
+
+img.save("/home/myw/drugvae/3D_model/data/best_molecules.png")
+print("🎉 图片已保存为 3D_model/data/best_molecules.png，快去看看！")
